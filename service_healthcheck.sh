ea8ea66a57f867001caba5919b054795258b80c0
pmshrik <142974574+pmshrik@users.noreply.github.com>
2025-10-20 10:50:53 +0530

    Create service_healthcheck.sh

---
+++ service_healthcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SERVICE="nginx"
+if systemctl is-active --quiet $SERVICE; then
+  echo "$SERVICE is running"
+else
+  echo "$SERVICE is not running, restarting..."
+  sudo systemctl restart $SERVICE
+fi
